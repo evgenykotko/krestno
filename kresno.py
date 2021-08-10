b3152965e05b3aaf432cdaa09da537c821b89e5f
evgenykotko <kotko.evgeny@gmail.com>
2021-08-11 03:11:41 +0400

    Update kresno.py

--- kresno.py
+++ kresno.py
@@ -1,19 +1,20 @@
-# -*- coding: utf-8 -*-
-def create():
-    pole = [["."]*3 for i in range(3)]
-    return pole
 
-def pole_reload(pole):
+def menu():
+    if input("Хотите сыграть? (y / n): ") == "y":
+        pole_reload()
+
+    else:
+        print("Жаль, всего хорошего!")
+        exit()
+
+def pole_reload():
     print("\n" * 10)
     for i in range(len(pole)):
         for j in range(len(pole[i])):
             print(pole[i][j], end=' ')
         print()
-    proverka_1(pole)
-    proverka(pole)
-    hod(pole)
 
-def hod(pole):
+def hod():
     while True:
         coord = input("Введите координаты: ").split()
         if len(coord) != 2:
@@ -25,7 +26,9 @@ def hod(pole):
         if not (a.isdigit()) or not (b.isdigit()):
             print("Введите числа!")
             continue
+
         a, b = int(a), int(b)
+
         if 1 > a > 3 and 1 > b > 3:
             print("Координаты вне игрового поля, попробуйте еще раз")
             continue
@@ -33,52 +36,45 @@ def hod(pole):
         if pole[a - 1][b - 1] != ".":
             print("Квадрат занят, попробуйте другой")
             continue
+
         return a, b
-def proverka_1(pole):
-    count = 0
-    for i in range(len(pole)):
-        for j in range(len(pole[i])):
-            if pole[i][j] == ".":
-                count += 1
-    if count == 0:
-        print("Ходы закончились")
-        menu()
 
+def proverka():
+    winner = [((0, 0), (0, 1), (0, 2)), ((0, 0), (1, 1), (2, 2)), ((0, 2), (1, 1), (2, 0)), ((1, 0), (1, 1), (1, 2)),
+              ((2, 0), (2, 1), (2, 2)), ((0, 0), (1, 0), (2, 0)), ((0, 1), (1, 1), (2, 1)), ((0, 2), (1, 2), (2, 2))]
+    for cord in winner:
+        x, y, z = cord[0],cord[1],cord[2]
+        if pole[x[0]][x[1]]==pole[y[0]][y[1]]==pole[z[0]][z[1]]!=".":
+            pole_reload()
+            print(f"Победили {pole[x[0]][x[1]]}!")
+            return True
+    return False
 
-def proverka(pole):
-    if pole[0][0] == pole[1][1] == pole[2][2] != ".":
-        print("Победа!")
-        menu()
-    elif pole[0][2] == pole[1][1] == pole[2][0] != ".":
-        print("Победа!")
-        menu()
-    elif pole[0][0] == pole[0][1] == pole[0][2] != ".":
-        print("Победа!")
-        menu()
-    elif pole[1][0] == pole[1][1] == pole[1][2] != ".":
-        print("Победа!")
-        menu()
-    elif pole[2][0] == pole[2][1] == pole[2][2] != ".":
-        print("Победа!")
-        menu()
-    elif pole[0][0] == pole[1][0] == pole[2][0] != ".":
-        print("Победа!")
-        menu()
-    elif pole[0][1] == pole[1][1] == pole[2][1] != ".":
-        print("Победа!")
-        menu()
-    elif pole[0][2] == pole[1][2] == pole[2][2] != ".":
-        print("Победа!")
-        menu()
-    else:
-        hod(pole)
-
-def menu():
-    pole=create()
-    if input("Хотите сыграть? (y / n): ") == "y":
-        pole_reload(pole)
-    else:
-        print("Жаль, всего хорошего!")
-        exit()
-
+count = 0
+pole = [["."]*3 for i in range(3)]
 menu()
+
+while True:
+    count += 1
+
+    pole_reload()
+
+    if count % 2 == 1:
+        print("Ходит крестик")
+    else:
+        print("Ходит нолик")
+
+    a, b = hod()
+
+    if count % 2 == 1:
+        pole[a - 1][b - 1] = "X"
+    else:
+        pole[a - 1][b - 1] = "0"
+
+    if proverka():
+        break
+
+
+    if count == 9:
+        print("Ходы закончились")
+        break
